912eefc5014d9d4b62d2e1d0655ae4909ec7e120
chawitW <chawit.wan@gmail.com>
2021-03-12 12:11:57 +0700

    Bigger app icon and adding chart

--- lib/appBar/analytic_screen.dart
+++ lib/appBar/analytic_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 void main() {
   runApp(MaterialApp(home: AnalyticsPage()));
@@ -93,11 +94,13 @@ class _SettingPageState extends State<AnalyticsPage> {
                                                       .data.documents.length,
                                                   itemBuilder:
                                                       (context, index) {
-                                                         DocumentSnapshot
-                                                  documentSnapshot = snapshots
-                                                      .data.documents[index];
-                                                    return Text(documentSnapshot[
-                                                        "todoTitle"]);
+                                                    DocumentSnapshot
+                                                        documentSnapshot =
+                                                        snapshots.data
+                                                            .documents[index];
+                                                    return Text(
+                                                        documentSnapshot[
+                                                            "todoTitle"]);
                                                   },
                                                 ),
                                               );
@@ -120,7 +123,27 @@ class _SettingPageState extends State<AnalyticsPage> {
                 child: ExpansionTile(
                   // initiallyExpanded: true,
                   title: Text("Financial graph"),
-                  children: [Text("This is a graph")],
+                  children: [
+                    BarChart(BarChartData(barGroups: [
+                      for (int j = 0; j < 7; j++)
+                        BarChartGroupData(x: j + 1, barRods: [
+                          BarChartRodData(
+                              y: (j + 1) * 5.0,
+                              colors: [
+                                Colors.redAccent
+                                // Colors.greenAccent
+                              ],
+                              width: 3),
+                          BarChartRodData(
+                              y: (j + 2) * 4.0,
+                              colors: [
+                                // Colors.redAccent
+                                Colors.greenAccent
+                              ],
+                              width: 3)
+                        ])
+                    ]))
+                  ],
                 ),
               ),
               Card(
@@ -128,7 +151,47 @@ class _SettingPageState extends State<AnalyticsPage> {
                 child: ExpansionTile(
                   // initiallyExpanded: true,
                   title: Text("Time boxing pie chart"),
-                  children: [Text("This is a chart")],
+                  children: [
+                    Text("PieChart is below"),
+                    Center(
+                      heightFactor: 10,
+                      child: ListTile(
+                        title: PieChart(PieChartData(
+                            startDegreeOffset: 180,
+                            sectionsSpace: 1,
+                            centerSpaceRadius: 0,
+                            // centerSpaceColor: Colors.white,
+                            borderData: FlBorderData(
+                              show: false,
+                            ),
+                            sections: [
+                              PieChartSectionData(
+                                title: '30%',
+                                value: 30,
+                                radius: 80,
+                                color: Colors.redAccent.withOpacity(0.7),
+                                titleStyle: TextStyle(color: Colors.black12),
+                                titlePositionPercentageOffset: 0.55,
+                              ),
+                              PieChartSectionData(
+                                  title: '20%',
+                                  radius: 80,
+                                  value: 20,
+                                  color: Colors.blueAccent),
+                              PieChartSectionData(
+                                  title: '10%',
+                                  radius: 80,
+                                  value: 10,
+                                  color: Colors.greenAccent),
+                              PieChartSectionData(
+                                  title: '40%',
+                                  radius: 80,
+                                  value: 40,
+                                  color: Colors.yellowAccent),
+                            ])),
+                      ),
+                    )
+                  ],
                 ),
               ),
             ],
